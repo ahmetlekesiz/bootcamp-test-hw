559a169432231d42e488c001155788460331ec39
Ahmet Lekesiz <ahmetlekesiz@gmail.com>
2020-09-19 00:17:17 +0300

    Implement test for WhenAllWordsAreNotUnique

--- src/test/java/NumberOfUniqueWordsTests.java
+++ src/test/java/NumberOfUniqueWordsTests.java
@@ -16,5 +16,18 @@ public class NumberOfUniqueWordsTests {
         assertThat(result).isEqualTo(4);
     }
 
+    @Test
+    public void getNumberOfUniqueWords_WhenAllWordsAreNotUnique_ShouldReturnNumberOfWords(){
+        // Arrange
+        NumberOfUniqueWords sut = new NumberOfUniqueWords();
+
+        // Act
+        String input = "This is a test and a good test.";
+        int result = sut.getNumberOfUniqueWords(input);
+
+        // Verify
+        assertThat(result).isEqualTo(6);
+    }
+
 
 }
